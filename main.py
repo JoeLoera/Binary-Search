23646f7e4d5bc8efad7cb5a42af531910b14dc9c
Nova Loera <60495917+JoeLoera@users.noreply.github.com>
2022-07-06 03:16:21 +0000

    Completed the final if statement that determines the length value and time to find

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+import time
 #divide and conquer algorithm to search a list 
 #assume we have a list from least to greatest. We want to see if a target is in the list and we need the targets location
 #in binary search we can go to the middle of the list and ask if the target is equal or less than OR equal or greater than that middle element
@@ -19,6 +21,7 @@ def binary_search(l,traget, low=None, high=None):
 
   if high < low:
     return -1
+
   #example l = [1,3,5,10,12] #should return 3
   midpoint = (low+high) // 2 #2
 
@@ -34,7 +37,26 @@ def binary_search(l,traget, low=None, high=None):
 
 
 if __name__ == '__main__':
-  l = [1,3,5,10,12]
-  target = 10
-  print(naive_search(l,target))
-  print(binary_search(l,target))
+  #l = [1,3,5,10,12]
+  #target = 10
+  #print(naive_search(l,target))
+  #print(binary_search(l,target))
+
+  length = 10000
+  #build a sorted list of length 10000
+  sorted_list = set()
+  while len(sorted_list) < length:
+    sorted_list.add(random.randint(-3*length, 3*length))
+  sorted_list = sorted(list(sorted_list))
+  
+  start = time.time()
+  for target in sorted_list:
+    naive_search(sorted_list, target)
+  end = time.time()
+  print("Naive search time: ", (end-start)/length, "seconds")
+
+  start = time.time()
+  for target in sorted_list:
+    binary_search(sorted_list, target)
+  end= time.time()
+  print("Binary search time: ", (end-start)/length, "seconds")
